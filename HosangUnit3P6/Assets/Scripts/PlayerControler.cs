18eabfd831c838be7bdf4815d5036cd56caaca66
Ojcitrius32 <732537@student.dusd.net>
2026-03-23 13:59:37 -0700

    started project
    going smothley so far

---
+++ HosangUnit3P6/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
